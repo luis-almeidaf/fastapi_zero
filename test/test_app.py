94cb6f8724cc54dcfbc9fec7efd2fd1c80802d98
Luis <luis.almeida,faria@gmail.com>
2026-08-02 16:48:02 -0300

    feat: Adicionando shema de mensagem

--- test/test_app.py
+++ test/test_app.py
@@ -12,3 +12,12 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {"message": "Olá mundo"}
+
+
+def test_root_deve_retornar_html_com_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get("/ola_mundo")
+
+    assert response.status_code == HTTPStatus.OK
+    assert "<h1> Olá Mundo </h1>" in response.text
